c9a7e8c6afdeee34dacd8ffc61e6250bce1c42d8
Digambaran <arjunsreekumar.1@gmail.com>
2022-12-22 13:58:19 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Sample_login_form_page from './sample_login_form_page'
+export default function App() {
+  return (
+    <div>
+      <Sample_login_form_page/>
+    </div>
+  )
+}
